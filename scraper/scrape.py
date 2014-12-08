f8b4b7dd02ce3ac5972d77ec03be70a61a3f54dd
William Jagels <will.jagels@gmail.com>
2014-12-08 23:31:44 +0000

    Now the IDs are flowing in.  Damn, python is working so well.

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -3,16 +3,22 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 from pymongo import MongoClient
 import json
+from pprint import pprint
 __author__ = 'William'
 
 def main():
     global b
     b = webdriver.PhantomJS()
+    b.delete_all_cookies()
     b.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
+    print("phase 1")
     accept = b.find_element_by_name("disclaimerFM")
     accept.submit()
     b.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
-    listPrison("ALBANY TC")
+    json_data = json.load(open('prisonlist.json'))
+    pprint(json_data)
+    for prison in json_data:
+        listPrison(prison)
     
     
 def listPrison(location):
@@ -27,7 +33,10 @@ def listPrison(location):
     #maxField.select_by_value("150")
     #b.execute_script("document.getElementById('RecordsPerPage').value = '150'")
     form.submit()
-    print(b.page_source)
+    ids = b.find_elements_by_name("vRecNo")
+    for gcdid in ids:
+        print(gcdid.get_attribute('value'))
+    #print(b.page_source)
 
 
 b = 0
