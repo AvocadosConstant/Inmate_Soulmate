67e4c25d950034103e36a2782346a6a6098f3374
William Jagels <will.jagels@gmail.com>
2014-12-09 00:18:14 +0000

    Now pulls data for the individual prisoners, next is to toss them into the database

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -4,25 +4,21 @@ from selenium.webdriver.support.ui import Select
 from pymongo import MongoClient
 import json
 from pprint import pprint
+from multiprocessing import Pool
 __author__ = 'William'
 
 def main():
     global b
-    b = webdriver.PhantomJS()
-    b.delete_all_cookies()
-    b.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
-    print("phase 1")
-    accept = b.find_element_by_name("disclaimerFM")
-    accept.submit()
-    b.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
     json_data = json.load(open('prisonlist.json'))
     pprint(json_data)
     for prison in json_data:
+        print("Starting pull of " + prison + "!")
         listPrison(prison)
-    
+
     
 def listPrison(location):
     global b
+    global pool
     b.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
     radioBtn = b.find_element_by_id("byOther")
     locField = Select(b.find_element_by_id("vCurrentInstitution"))
@@ -35,10 +31,40 @@ def listPrison(location):
     form.submit()
     ids = b.find_elements_by_name("vRecNo")
     for gcdid in ids:
-        print(gcdid.get_attribute('value'))
+        pullPrisoner(gcdid.get_attribute('value'))
     #print(b.page_source)
 
+def pullPrisoner(prisonerID):
+    global b2
+    print(prisonerID)
+    b2.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
+    form = b2.find_element_by_id("OffenderQueryForm")
+    identifier = Select(b2.find_element_by_id("vUnoCaseNoRadioButton"))
+    identifier.select_by_value("UNO_NO")
+    textBox = b2.find_element_by_id("vOffenderId")
+    textBox.send_keys(prisonerID)
+    form = b2.find_element_by_id("OffenderQueryForm")
+    form.submit()
 
-b = 0
+def initializeBrowsers():
+    global b
+    global b2
+    bs = []
+    bs.append(b)
+    bs.append(b2)
+    for br in bs:
+        br.delete_all_cookies()
+        br.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
+        accept = br.find_element_by_name("disclaimerFM")
+        accept.submit()
+        br.get("http://www.dcor.state.ga.us/GDC/OffenderQuery/jsp/OffQryForm.jsp")
+        print("Fired up a browser!")
+    b = bs[0]
+    b2 = bs[1]
+
+pool = Pool(processes=1)
+b = webdriver.PhantomJS()
+b2 = webdriver.PhantomJS()
+initializeBrowsers()
 client = MongoClient()
 main()
